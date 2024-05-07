24dc9756d486e662753db292899dbf16a7ab8fab
plOloV2 <ak.dziagwa@gmail.com>
2024-05-07 19:16:28 +0200

    pierwszy commit

---
+++ src/Ant.java
@@ -0,0 +1,3 @@
+public class Ant {
+    //po niej bedą dziedziczyć robotnice i lider
+}
---
+++ src/Symulacja.java
@@ -0,0 +1,14 @@
+public class Symulacja {
+    public static void main(String[] args) throws Exception {
+
+        short number_of_ants;       //min 10
+        short board_size;           //min 100, max 1000
+        boolean end = false;        //ends simulation
+
+
+        while(!end){
+
+        }
+
+    }
+}
