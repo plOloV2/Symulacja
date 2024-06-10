72e18629c1ef45dc9fc7d1698f451b25b407d178
plOloV2 <ak.dziagwa@gmail.com>
2024-06-10 22:14:08 +0200

    worker optymalization

--- src/Ant_worker.java
+++ src/Ant_worker.java
@@ -8,33 +8,25 @@ public class Ant_worker extends Ant{
     }
     Point last_position;
 
-    public boolean simulate(Point end_position, Point previouse_ant_position, Obstackle terrain){      //simulates ant movement, return true if ant has reached food source
+    public boolean simulate(Point end_position, Point previouse_ant_position, Obstackle terrain){      //simulates ant movement, return true if ant has reached food source                              
 
-        if(this.position.X_pos()  >= (end_position.X_pos()-2) &&
-        this.position.X_pos()  <= (end_position.X_pos()+2) &&
-        this.position.Y_pos()  >= (end_position.Y_pos()-2) &&
-        this.position.Y_pos()  <= (end_position.Y_pos()+2)){                //checks if ant is at food source position
-            setCanSimulate();                                               // if ant is at food source dont draw it
-            return true;
-        }                                
-
-        int x = previouse_ant_position.X_pos() - this.position.X_pos();     //calculates x and y diference betwen its position and previos ant position
+        int x = previouse_ant_position.X_pos() - this.position.X_pos();             //calculates x and y diference betwen its position and previos ant position
         int y = previouse_ant_position.Y_pos() - this.position.Y_pos();
         
-        float distance = (float)Math.sqrt(x*x+y*y);                         //calculates distance betwen its position and previos ant position
+        float distance = (float)Math.sqrt(x*x+y*y);                                 //calculates distance betwen its position and previos ant position
 
-        if(this.position.X_pos()  == end_position.X_pos() && this.position.Y_pos()  == end_position.Y_pos() && distance <= 2){        //checks if previous ant is at food source and if distance to it is less than 2
-            this.position = end_position;                                   //if yes, return true
-            setCanSimulate();                                               //if ant is at food source dont draw it
+        if(previouse_ant_position.X_pos()  == end_position.X_pos() && previouse_ant_position.Y_pos()  == end_position.Y_pos() && distance <= 4){        //checks if previous ant is at food source and if distance to it is less than 2
+            this.position = end_position;                                           //if yes, return true
+            setCanSimulate();                                                       //if ant is at food source dont draw it
             return true;
         }
 
         if(distance > 30){
-            x = Math.round(x * (8 / distance));                                 //scales x and y movement to move aproximetly 8 tiles
+            x = Math.round(x * (8 / distance));                                     //if distance to previouse ant is bigger than 30 units, scales x and y movement to move aproximetly 8 tiles
             y = Math.round(y * (8 / distance));
         }
         else{
-            x = Math.round(x * (4 / distance));                                 //scales x and y movement to move aproximetly 4 tiles
+            x = Math.round(x * (4 / distance));                                     //if distance to previouse ant is smaller than 30 units, scales x and y movement to move aproximetly 4 tiles
             y = Math.round(y * (4 / distance));
         }
 
@@ -53,16 +45,16 @@ public class Ant_worker extends Ant{
             distance = (float)Math.sqrt(x*x+y*y);                                   //calculates distance etwen its position and previos ant position
     
             if(distance > 30){
-                x = Math.round(x * (8 / distance));                                 //scales x and y movement to move aproximetly 8 tiles
+                x = Math.round(x * (8 / distance));                                 //if distance to previouse ant is bigger than 30 units, scales x and y movement to move aproximetly 8 tiles
                 y = Math.round(y * (8 / distance));
             }
             else{
-                x = Math.round(x * (4 / distance));                                 //scales x and y movement to move aproximetly 4 tiles
+                x = Math.round(x * (4 / distance));                                 //if distance to previouse ant is smaller than 30 units, scales x and y movement to move aproximetly 4 tiles
                 y = Math.round(y * (4 / distance));
-            }          
+            }         
         }
 
-        this.position.change_coordinates(x, y);               //changes ants position
+        this.position.change_coordinates(x, y);                                     //changes ants position
 
         last_position = position;
 
